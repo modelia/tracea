e59f894e6f37f1f9d571b031a82322cd4485c9da
Edouard R. Batot <batotedo@iro.umontreal.ca>
2021-12-14 15:47:15 +0100

    ADDED copyrights

--- 1-dsl-tracea/Tracea/src/tracea/relationtyping/impl/Section2PoSImpl.java
+++ 1-dsl-tracea/Tracea/src/tracea/relationtyping/impl/Section2PoSImpl.java
@@ -1,3 +1,32 @@
+/*****************************************************************************
+
+* Copyright (c) 2015, 2018 CEA LIST, Edouard Batot
+
+*
+
+* All rights reserved. This program and the accompanying materials
+
+* are made available under the terms of the Eclipse Public License 2.0
+
+* which accompanies this distribution, and is available at
+
+* https://www.eclipse.org/legal/epl-2.0/
+
+*
+
+* SPDX-License-Identifier: EPL-2.0
+
+*
+
+* Contributors:
+
+* CEA LIST - Initial API and implementation
+
+* Edouard Batot (UOC SOM) ebatot@uoc.edu 
+
+*****************************************************************************/
+
+
 /**
  */
 package tracea.relationtyping.impl;
